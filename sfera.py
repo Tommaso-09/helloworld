281f12058b0d3f9389a698c42f2d9cbb2887ef45
Tommaso-09 <tommaso230970@gmail.com>
2017-04-23 21:41:00 +0200

    Ho aggiunto il punto vicino i numeri così escono con la virgola

--- sfera.py
+++ sfera.py
@@ -3,7 +3,7 @@ import math
 print "Inserisci il raggio : "
 raggio = input()
 
-superficie = 4 * math.pi * (raggio * raggio)
-volume = 4 / 3 * math.pi * (raggio * raggio * raggio)
+superficie = 4. * math.pi * (raggio * raggio)
+volume = 4. / 3. * math.pi * (raggio * raggio * raggio)
 
 print "La superficie della sfera e' : ", superficie , "e il volume e' : ", volume
