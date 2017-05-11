1c4c7c4f9254c4fa3d681cfe78b90289376005f8
alunno <alunno@lab03-14>
2017-05-11 19:49:49 +0200

    Fatto

---
+++ primogrado.py
@@ -0,0 +1,12 @@
+print "Inserisci a e b per svolgere un equazione di primo grado "
+a = input("Inserisci a : ")
+b = input("Inserisci b : ")
+
+if a == 0 :
+    
+    print " a e' uguale a 0 , quidi il risultato e' impossibile . "
+
+else :
+
+    x = b/a
+    print " L'incognita x con a ",a," e b ",b," ha valore : ",x
