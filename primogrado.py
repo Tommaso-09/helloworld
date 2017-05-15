379427b9d9ef1867e0722299840da39ba3b5a1be
Tommaso-09 <tommaso230970@gmail.com>
2017-05-15 17:33:19 +0200

    Fatto

--- primogrado.py
+++ primogrado.py
@@ -2,11 +2,16 @@ print "Inserisci a e b per svolgere un equazione di primo grado "
 a = input("Inserisci a : ")
 b = input("Inserisci b : ")
 
-if a == 0 :
-    
-    print " a e' uguale a 0 , quidi il risultato e' impossibile . "
+if a==0 and b==0 :
+
+    print " a e b sono uguali a 0 , quindi l'equazione ammette infinite soluzioni. "
+
+elif a == 0 :
+
+    print " a e' uguale a 0 , quidi il risultato dell'equazione e' impossibile . "
+
 
 else :
 
-    x = b/a
+    x = -float(b)/float(a)
     print " L'incognita x con a ",a," e b ",b," ha valore : ",x
